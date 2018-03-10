3b1710d356a66ccde24d3194703730a481ffe9c0
Mandi Cai <mandi@biobright.org>
2018-03-10 13:28:44 -0500

    Adjustments for world map (viewBox, translation) Took out path that chunks all subunits together

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ var svg = d3.select("#map").append("svg")
     // defaults to meet (aspect ratio is preserved, entire viewBox is visible)
     .attr("preserveAspectRatio", "xMinYMin meet")
     // y-axis is the same scale
-    .attr("viewBox", "0 0 960 325")
+    .attr("viewBox", "0 20 800 600")
     .classed("svg-content", true);
 
 let boundaries
@@ -19,7 +19,7 @@ dataFiles.forEach(function(url,i) {
     })
 })
 
-fetch('data/afg_irn_col_kor_jpn.json')
+fetch('data/world_2000.json')
   .then((res) => res.json()) // .json() returns a promise that resolves with the result of parsing the text as JSON
   .then((data) => { // do something with the JSON data
     boundaries = data
@@ -29,17 +29,19 @@ fetch('data/afg_irn_col_kor_jpn.json')
 
   // store geomercator projection
   var projection = d3.geoMercator()
-      .translate([width / 2 - 225, height / 2]) // translate some pixels
-      .scale(150)
+      .translate([width / 2 - 100, height / 2 + 150]) // translate some pixels
+      .scale(100)
 
   // convert projection to a path
   var path = d3.geoPath()
       .projection(projection)
 
   // append the path to the svg for the country outlines
-  svg.append("path")
-      .datum(subunits) // bind multiple features
-      .attr("d", path) // d is the attribute for SVG paths
+  // This part attaches an entire map that groups together the country subunits as one large chunk
+  // Take out to access individual countries?
+  // svg.append("path")
+  //     .datum(subunits) // bind multiple features
+  //     .attr("d", path) // d is the attribute for SVG paths
 
   svg.selectAll(".subunit")
         .data(topojson.feature(boundaries, boundaries.objects.subunits).features)
