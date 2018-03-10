fb93fd3be9864f4e93dd6da5c7a5122e4a0ad639
Mandi Cai <mandi@biobright.org>
2018-03-10 16:28:32 -0500

    Fetched data successfully with d3! (trick was adding .then)

--- js/index.js
+++ js/index.js
@@ -15,11 +15,6 @@ let csvFiles = ['data/testdata.csv', 'data/DFscores.csv']
 
 var jsonPromises = jsonFiles.map(url => fetch(url));
 var csvPromises = csvFiles.map(url => fetch(url));
-//var responsePromises = responses.map(res => variables.push(res)))
-
-d3.json("data/afg_irn_col_kor_jpn.json").then(function(data) {
-  console.log(data); // [{"Hello": "world"}, …]
-});
 
 // Promise.all(jsonPromises)
 //   .then(responses => Promise.all(responses.map(res => res.json()))
@@ -41,11 +36,10 @@ d3.json("data/afg_irn_col_kor_jpn.json").then(function(data) {
 //       console.log(variables)
 //     })
 // })
+//var responsePromises = responses.map(res => variables.push(res)))
 
-fetch('data/world_2000.json')
-  .then((res) => res.json()) // .json() returns a promise that resolves with the result of parsing the text as JSON
-  .then((data) => { // do something with the JSON data
-    boundaries = data
+d3.json("data/world_2000.json").then(function(data) {
+  boundaries = data
 
   // topojson - feature collection
   var subunits = topojson.feature(boundaries, boundaries.objects.subunits)
@@ -75,66 +69,63 @@ fetch('data/world_2000.json')
         .attr("d", path)
 
    // fetch a CSV file and store the objects into an array
-  let descriptionsStatistics = []
-  d3.csv("/data/testdata.csv", function(data) {
-   descriptionsStatistics.push(data)
-  });
+  d3.csv("/data/testdata.csv").then(function(data) {
+    // create mouse over and mouse out functionality
+    svg.selectAll(".subunit")
+        .on("mouseover", function() {
+          d3.select(this) // Change the color of the country
+            .style('opacity', '0.75')
 
-  // create mouse over and mouse out functionality
-  svg.selectAll(".subunit")
-      .on("mouseover", function() {
-        d3.select(this) // Change the color of the country
-          .style('opacity', '0.75')
+          d3.select('#country-text')
+            .remove()
+          d3.select('#description-text') // Remove the previous text
+            .remove()
+          d3.select('#statistics-text')
+            .remove()
 
-        d3.select('#country-text')
-          .remove()
-        d3.select('#description-text') // Remove the previous text
-          .remove()
-        d3.select('#statistics-text')
-          .remove()
-
-        let countryClass = d3.select(this).attr("class").split(' ')[1] // Grab the class
-        let countryText
-        let descriptionText
-        let statisticsText
-        // Set the corresponding text
-        for (i=0;i<descriptionsStatistics.length;i++) {
-          if (descriptionsStatistics[i].CODE === countryClass) {
-            descriptionText = descriptionsStatistics[i].DESCRIPTION
-            statisticsText = descriptionsStatistics[i].STATISTICS
-            countryText = descriptionsStatistics[i].NAME
+          let countryClass = d3.select(this).attr("class").split(' ')[1] // Grab the class
+          let countryText
+          let descriptionText
+          let statisticsText
+          // Set the corresponding text
+          for (i=0;i<data.length;i++) {
+            if (data[i].CODE === countryClass) {
+              descriptionText = data[i].DESCRIPTION
+              statisticsText = data[i].STATISTICS
+              countryText = data[i].NAME
+            }
           }
-        }
 
-        // Append country name with the corresponding text
-        d3.select("#country")
-          .append('p')
-          .attr('id', 'country-text')
-          .text(countryText)
-          .style("opacity", "0")
-          .transition()
-          .style("opacity", "1")
-        // Append description text with the corresponding text
-        d3.select("#description")
-          .append('p')
-          .attr('id','description-text')
-          .text(descriptionText)
-          .style("opacity", "0")
-          .transition()
-          .style("opacity", "1")
-        // Append statistics text with the corresponding text
-        d3.select("#statistics")
-          .append('p')
-          .attr('id', 'statistics-text')
-          .text(statisticsText)
-          .style("opacity", "0")
-          .transition()
-          .style("opacity", "1")
-      })
-      .on("mouseout", function() {
-        d3.select(this).style('opacity', function (d) {
+          // Append country name with the corresponding text
+          d3.select("#country")
+            .append('p')
+            .attr('id', 'country-text')
+            .text(countryText)
+            .style("opacity", "0")
+            .transition()
+            .style("opacity", "1")
+          // Append description text with the corresponding text
+          d3.select("#description")
+            .append('p')
+            .attr('id','description-text')
+            .text(descriptionText)
+            .style("opacity", "0")
+            .transition()
+            .style("opacity", "1")
+          // Append statistics text with the corresponding text
+          d3.select("#statistics")
+            .append('p')
+            .attr('id', 'statistics-text')
+            .text(statisticsText)
+            .style("opacity", "0")
+            .transition()
+            .style("opacity", "1")
+        })
+        .on("mouseout", function() {
+          d3.select(this).style('opacity', function (d) {
         })
       })
+    })
   })
   .catch(function(error) {
     console.log("Error: " + error)
