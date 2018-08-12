0a6460e539ea8541d290ee2ceb94344a693ea373
Mandi Cai <mandi@biobright.org>
2018-08-12 17:40:43 -0400

    Made navbar responsive Resized map

---
+++ js/nav-bar.js
@@ -0,0 +1,10 @@
+d3.select(".mobile-icon").on("click", function() {
+  mobileMenuToggle()
+})
+
+function mobileMenuToggle() {
+  let navs = document.querySelectorAll('.nav-links')
+  console.log('clicked')
+  console.log(navs)
+  navs.forEach(nav => nav.classList.toggle('nav-links__show'))
+}
